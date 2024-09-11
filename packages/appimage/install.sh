7a3b9706692ddc8597c6460c8502c16824eb9e33
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-09-11 23:02:11 +0530

    refactor(appimage): change paths

--- packages/appimage/install.sh
+++ packages/appimage/install.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/AppImages
-
-wget -P ~/AppImages/ https://github.com/bpozdena/OneDriveGUI/releases/download/v1.0.3/OneDriveGUI-1.0.3_fix150-x86_64.AppImage
+wget -P ~/Downloads/ https://github.com/bpozdena/OneDriveGUI/releases/download/v1.0.3/OneDriveGUI-1.0.3_fix150-x86_64.AppImage
