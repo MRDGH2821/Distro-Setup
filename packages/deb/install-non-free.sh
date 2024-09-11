b77034c53e356e39eca2e22d5f894fee74a36f16
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-09-11 20:08:50 +0530

    refactor: skip already installed packages

--- packages/deb/install-non-free.sh
+++ packages/deb/install-non-free.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-sudo nala install --update -y -o APT::Get::AllowUnauthenticated=true \
-  "https://release.gitkraken.com/linux/gitkraken-amd64.deb" \
-  "https://cdn.akamai.steamstatic.com/client/installer/steam.deb"
+LINE="-------------------------------------------"
+
+function nala_install {
+  sudo nala install --update -y -o APT::Get::AllowUnauthenticated=true "$@"
+  echo $LINE
+}
+
+if command -v steam &>/dev/null; then
+  echo "Steam is already installed. Skipping"
+else
+  nala_install "https://cdn.akamai.steamstatic.com/client/installer/steam.deb"
+fi
+
+if command -v gitkraken &>/dev/null; then
+  echo "GitKraken is already installed. Skipping"
+else
+  nala_install "https://release.gitkraken.com/linux/gitkraken-amd64.deb"
+fi
