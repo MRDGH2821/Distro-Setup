90fe3610bc9856ad4521582390619b4703c8c902
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-09-11 22:47:48 +0530

    fix(appimage): use correct flag to download

--- packages/appimage/install.sh
+++ packages/appimage/install.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ~/AppImages
 
-wget -O ~/AppImages/ https://github.com/bpozdena/OneDriveGUI/releases/download/v1.0.3/OneDriveGUI-1.0.3_fix150-x86_64.AppImage
+wget -P ~/AppImages/ https://github.com/bpozdena/OneDriveGUI/releases/download/v1.0.3/OneDriveGUI-1.0.3_fix150-x86_64.AppImage
