501e7e94b5a31d9327e6cc11c2e1f9a09683a6b0
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-09-11 22:55:36 +0530

    feat(deb): remove firefox ppa

--- packages/deb/install-apps.sh
+++ packages/deb/install-apps.sh
@@ -12,22 +12,6 @@ function nala_install {
 # Add Repositories
 echo "Setting up repositories"
 
-## Firefox
-sudo install -d -m 0755 /etc/apt/keyrings
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc >/dev/null
-gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
-mozilla_source="deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main"
-if ! grep -q "$mozilla_source" /etc/apt/sources.list.d/mozilla.list; then
-  echo "$mozilla_source" | sudo tee -a /etc/apt/sources.list.d/mozilla.list >/dev/null
-fi
-echo '
-Package: *
-Pin: origin packages.mozilla.org
-Pin-Priority: 1000
-' | sudo tee /etc/apt/preferences.d/mozilla
-
-echo $LINE
-
 ## KeePassXC
 sudo add-apt-repository ppa:phoerious/keepassxc -y
 echo $LINE
