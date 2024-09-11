6920e3e2bb19088d6f9f5ba7c929f2de1e065edf
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-09-11 18:14:50 +0530

    fix: remove redundant line

--- packages/install.sh
+++ packages/install.sh
@@ -8,7 +8,6 @@ export PATH=$HOME/.local/bin:$PATH
 
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
-bash "$SCRIPT_DIR"/preinstall-steps.sh
 bash "$SCRIPT_DIR"/auto/install-apps.sh
 bash "$SCRIPT_DIR"/deb/install.sh
 bash "$SCRIPT_DIR"/flatpak/install-apps.sh
